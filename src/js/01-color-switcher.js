051e1cc3d3b4aadd9f10d7b94b32018ce28dee31
Vitalii Mazur <vitalii.mazur.work@gmail.com>
2022-09-03 22:39:32 +0300

    task 1 completed

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -7,7 +7,7 @@ const refs = {
 let timerId = null;
 
 refs.buttonStart.addEventListener('click', onButtonStartClick);
-refs.buttonStop.addEventListener('clicl', onButtonStopClick);
+refs.buttonStop.addEventListener('click', onButtonStopClick);
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
